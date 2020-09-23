facc30877fe8edbed96d4bb87bbc0ce64c09e71f
Calvin Kam <calvinkam123@gmail.com>
2020-09-23 10:52:19 +0800

    Initial Commit

---
+++ train_data/split.py
@@ -0,0 +1,45 @@
+import glob
+import os
+import sys
+
+print(sys.argv[1])
+# import Tkinter
+# import Tkconstants
+# import tkFileDialog
+# while True:
+#     print("Please select your image directory.")
+#     current_dir = tkFileDialog.askdirectory()
+#     if current_dir == None or current_dir == "":
+#         print("You must select a directory.")
+#         continue
+#     break
+# current_dir = os.getcwd()+"/darknet/train_data/coke"
+base_url = os.getcwd() + "/"
+current_dir = base_url + sys.argv[1]
+print(current_dir)
+
+# Percentage of images to be used for the test set
+percentage_test = 10
+# Create and/or truncate train.txt and test.txt
+file_train = open(base_url + 'train.txt', 'w')
+file_test = open(base_url + 'test.txt', 'w')
+# Populate train.txt and test.txt
+counter = 1
+index_test = round(100 / percentage_test)
+for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):
+    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+    if counter == index_test:
+        counter = 1
+        file_test.write(current_dir + "/" + title + '.jpg' + "\n")
+    else:
+        file_train.write(current_dir + "/" + title + '.jpg' + "\n")
+        counter = counter + 1
+
+for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.png")):
+    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+    if counter == index_test:
+        counter = 1
+        file_test.write(current_dir + "/" + title + '.png' + "\n")
+    else:
+        file_train.write(current_dir + "/" + title + '.png' + "\n")
+        counter = counter + 1
